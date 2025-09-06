9a88a163ab35d3013556a91d6311e68d26e72e65
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2025-09-06 12:01:33 -0700

    Fix YAML anchors for database config

--- backend/test/controllers/stations_controller_test.rb
+++ backend/test/controllers/stations_controller_test.rb
@@ -1,13 +1,17 @@
 require "test_helper"
 
 class StationsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @station = Station.create!(name: "Test Station")
+  end
+
   test "should get index" do
-    get stations_index_url
+    get stations_url, as: :json   # <--- tell Rails to request JSON
     assert_response :success
   end
 
   test "should get show" do
-    get stations_show_url
+    get station_url(@station), as: :json   # <--- request JSON
     assert_response :success
   end
 end
