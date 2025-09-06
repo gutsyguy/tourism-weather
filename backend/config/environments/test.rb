e667671cbe28664fa4439e433ce8c9f56e614496
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2025-09-06 14:03:16 -0700

    e

--- backend/config/environments/test.rb
+++ backend/config/environments/test.rb
@@ -50,4 +50,9 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+  # config/environments/test.rb
+config.action_cable.disable_request_forgery_protection = true
+config.action_cable.url = nil
+config.action_cable.mount_path = nil
+
 end
