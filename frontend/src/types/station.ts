56fda7e5a53045738715d435c703fbd5be647b08
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2025-09-07 12:20:42 -0700

    e

---
+++ frontend/src/types/station.ts
@@ -0,0 +1,26 @@
+import z from "zod";
+
+export const zStation = z.object({
+  station: z.string().nonempty(),
+  latitude: z.float64(),
+  longitude: z.float64(),
+  elevation: z.float32(),
+  station_name: z.string().nonempty(),
+  station_network: z.string().nonempty(),
+  timezone: z.string().nonempty(),
+});
+
+export interface Station {
+  station: string;
+  latitude: Float16Array;
+  longitude: Float16Array;
+  elevation: Float16Array;
+  station_name: string;
+  station_network: string;
+  timezone: string;
+}
+
+export interface Stations {
+  data: Station[];
+  corrupted_records: number;
+}
